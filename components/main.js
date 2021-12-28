e4c8cea280e00b12e9cb4ce3b510cae2538b1b93
Rishi Mohan <iamrishi.ms@gmail.com>
2021-12-28 20:30:42 +0530

    Added options

--- components/main.js
+++ components/main.js
@@ -2,6 +2,14 @@ import { useState } from "react";
 
 export default function Main() {
   const [blob, setBlob] = useState({src: null, w: 0, h: 0});
+  const [options, setOptions] = useState({
+    aspectRatio: "aspect-auto",
+    theme: "bg-white",
+    padding: "p-0",
+    rounded: "rounded-none",
+    shadow: "shadow-none",
+    
+  });
 
   const onPaste = event => {
     var items = (event.clipboardData || event.originalEvent.clipboardData).items;
@@ -13,7 +21,6 @@ export default function Main() {
         var blob = item.getAsFile();
         var reader = new FileReader();
         reader.onload = function(event){
-          // console.log(event.target.result); // data url!
           console.log("Event", event)
           setBlob({...blob, src: event.target.result});
         }
@@ -25,39 +32,126 @@ export default function Main() {
   const renderOptions = () => {
     return (
       <div className="fixed top-0 left-0 flex items-center justify-center w-full">
-        <div className="inline-flex px-8 py-3 mt-10 bg-gray-300/50 backdrop-blur rounded-xl">
-          Options
+        <div className="inline-flex px-8 py-3 mt-10 space-x-8 border border-gray-300 border-gray-400 shadow-xl bg-gray-400/50 dark:bg-gray-700/60 backdrop-blur rounded-xl dark:border-gray-500 shadow-black/20">
+          <div className="">
+            <div className="pb-2 text-sm font-semibold dark:text-white">Aspect Ratio</div>
+            <div>
+              <select
+                className="cursor-pointer"
+                onChange={(e) =>
+                  setOptions({ ...options, aspectRatio: e.target.value })
+                }
+              >
+                <option value="aspect-auto">Auto</option>
+                <option value="aspect-square">Square</option>
+                <option value="aspect-video">9:16</option>
+              </select>
+            </div>
+          </div>
+          <div className="">
+            <div className="pb-2 text-sm font-semibold dark:text-white">Padding</div>
+            <div>
+              <select
+                className="cursor-pointer"
+                onChange={(e) =>
+                  setOptions({ ...options, padding: e.target.value })
+                }
+              >
+                <option value="p-0">None</option>
+                <option value="p-10">Small</option>
+                <option value="p-20">Medium</option>
+                <option value="p-32">Large</option>
+              </select>
+            </div>
+          </div>
+          <div className="">
+            <div className="pb-2 text-sm font-semibold dark:text-white">Theme</div>
+            <div>
+              <select
+                className="cursor-pointer"
+                onChange={(e) =>
+                  setOptions({ ...options, theme: e.target.value })
+                }
+              >
+                <option value="bg-white">White</option>
+                <option value="bg-black">Black</option>
+                <option value="bg-gradient-to-br from-red-500 to-pink-600">
+                  Evil
+                </option>
+                <option value="bg-gradient-to-br from-green-100 to-yellow-100">
+                  Heaven
+                </option>
+              </select>
+            </div>
+          </div>
+          <div className="">
+            <div className="pb-2 text-sm font-semibold dark:text-white">Rounded Corners</div>
+            <div>
+              <select
+                className="cursor-pointer"
+                onChange={(e) =>
+                  setOptions({ ...options, rounded: e.target.value })
+                }
+              >
+                <option value="rounded-none">None</option>
+                <option value="rounded-lg">Small</option>
+                <option value="rounded-xl">Medium</option>
+                <option value="rounded-3xl">Large</option>
+              </select>
+            </div>
+          </div>
+          <div className="">
+            <div className="pb-2 text-sm font-semibold dark:text-white">Shadow</div>
+            <div>
+              <select
+                className="cursor-pointer"
+                onChange={(e) =>
+                  setOptions({ ...options, shadow: e.target.value })
+                }
+              >
+                <option value="shadow-none">None</option>
+                <option value="shadow-lg">Small</option>
+                <option value="shadow-xl">Medium</option>
+                <option value="shadow-2xl">Large</option>
+              </select>
+            </div>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 
   return (
     <div
-      className="flex items-center justify-center h-full min-h-screen p-10 bg-white"
+      className="flex items-center justify-center h-full min-h-screen p-10 bg-gradient-to-br from-slate-200 to-slate-400 dark:from-gray-900 dark:to-gray-900/90"
       onPaste={onPaste}
     >
       {blob?.src ? (
         <div
           style={blob?.w ? { width: blob?.w / window.devicePixelRatio } : {}}
-          className="max-w-[60vw] mt-20 p-10 bg-gradient-to-br from-green-200 to-yellow-200 rounded-xl"
+          className={
+            "duration-300 ease-in-out flex items-center justify-center max-w-[60vw] mt-20 rounded-xl " +
+            options?.theme +
+            " " +
+            options?.aspectRatio +
+            " " +
+            options?.padding
+          }
         >
-          {/* {console.log("Width", blob.naturalwidth)} */}
           <img
             src={blob?.src}
-            className="shadow-xl rounded-xl"
+            className={`${options?.shadow} ${options?.rounded}`}
             onLoad={(e) => {
               setBlob({
                 ...blob,
                 w: e.target.naturalWidth,
                 h: e.target.naturalHeight,
               });
-              console.log("Img e", e);
             }}
           />
         </div>
       ) : (
-        <span className="text-3xl opacity-50">
+        <span className="text-3xl opacity-50 dark:text-white">
           Paste your screenshot, or click to upload
         </span>
       )}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import 'styles/app.scss'
+import Head from "next/head";
+import "styles/app.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;600;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
