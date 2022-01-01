6e217220bf46a309bfecccf8c62f36dc1e4a475a
Rishi Mohan <iamrishi.ms@gmail.com>
2022-01-01 20:23:05 +0530

    Removed dark mode in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["pages/*.js", "components/*.js"],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
