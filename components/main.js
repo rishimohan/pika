8a21a10a75be13bcab01eb0c1b38c8c76c2f19f1
Rishi Mohan <iamrishi.ms@gmail.com>
2022-01-19 11:54:32 +0530

    Added buy coffee link, updates to dark mode neon

--- components/main.js
+++ components/main.js
@@ -10,6 +10,7 @@ import {
   TwitterIcon,
   GithubIcon,
   ColorPickerIcon,
+  CoffeeIcon,
 } from "ui/icons";
 
 const isValidHexColor = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
@@ -248,7 +249,7 @@ export default function Main() {
                   id="startColorPicker"
                   type="color"
                   className="absolute top-0 left-0 w-12 h-12 rounded-full opacity-0 cursor-pointer"
-                  value={options.customTheme.colorStart || "#000000"}
+                  value={options.customTheme.colorStart || "#222"}
                   onChange={(e) =>
                     setOptions({
                       ...options,
@@ -261,10 +262,10 @@ export default function Main() {
                 />
                 <label
                   style={{
-                    backgroundColor: options?.customTheme?.colorStart || "#111",
+                    backgroundColor: options?.customTheme?.colorStart || "#222",
                   }}
                   htmlFor="startColorPicker"
-                  className="left-0 flex items-center justify-center w-12 w-full h-12 h-full rounded-full pointer-events-none text-white/50 group-hover:scale-[1.1] duration-100"
+                  className="left-0 flex items-center justify-center w-12 h-12 rounded-full pointer-events-none text-white/50 group-hover:scale-[1.1] duration-100"
                 >
                   <span className="font-mono text-xs text-white/80 drop-shadow">
                     Pick
@@ -309,7 +310,7 @@ export default function Main() {
                   id="startColorPicker"
                   type="color"
                   className="absolute top-0 left-0 w-12 h-12 rounded-full opacity-0 cursor-pointer"
-                  value={options.customTheme.colorEnd || "#000000"}
+                  value={options.customTheme.colorEnd || "#222"}
                   onChange={(e) =>
                     setOptions({
                       ...options,
@@ -322,7 +323,7 @@ export default function Main() {
                 />
                 <label
                   style={{
-                    backgroundColor: options?.customTheme?.colorEnd || "#111",
+                    backgroundColor: options?.customTheme?.colorEnd || "#222",
                   }}
                   htmlFor="startColorPicker"
                   className="left-0 flex items-center justify-center w-12  h-12 rounded-full pointer-events-none text-white/50 group-hover:scale-[1.1] duration-100"
@@ -370,10 +371,10 @@ export default function Main() {
       <div className="sticky top-0 flex items-center lg:min-h-screen">
         <div
           className={classnames(
-            "p-6 lg:p-8 h-auto bg-white/90 dark:bg-[#000]/70 rounded-2xl ring-2 ring-pink-300 dark:ring-red-900/20 ring-offset-[2px] ring-offset-white dark:ring-offset-black shadow-lg shadow-gray-200 dark:shadow-black lg:max-h-screen w-full relative lg:min-h-[650px] mt-10 lg:mt-0"
+            "p-6 lg:p-8 h-auto bg-white/90 dark:bg-pink-600/60 rounded-2xl ring-1 ring-pink-300 dark:ring-pink-200/50 ring-offset-1 ring-offset-white dark:ring-offset-red-800 shadow-lg shadow-gray-200 dark:shadow-black lg:max-h-screen w-full relative lg:min-h-[650px] mt-10 lg:mt-0"
           )}
         >
-          <div className="absolute inset-0 w-full lg:h-full bg-gradient-to-br from-pink-200 dark:from-red-800/40 to-red-100 dark:to-pink-800/60 blur-xl dark:blur-2xl lg:scale-y-[1.05] scale-100 lg:scale-x-[1.1] lg:max-h-[calc(100vh-60px)] transform-gpu" />
+          <div className="absolute inset-0 w-full lg:h-full bg-gradient-to-br from-pink-400 dark:from-pink-500 dark:to-red-800 to-red-300 blur-xl dark:blur-md lg:scale-y-[1.05] scale-100 lg:scale-x-[1.1] dark:lg:scale-[1.05] lg:max-h-[calc(100vh-60px)] transform-gpu opacity-60" />
           <div className="relative flex flex-row flex-wrap items-start justify-start space-y-5 lg:items-start lg:flex-col lg:space-y-4">
             <div className="flex items-center justify-between w-full">
               <div className="text-sm font-semibold dark:text-white">
@@ -435,7 +436,7 @@ export default function Main() {
                 <div className="relative">
                   <div
                     onClick={() => setBGPicker(!bgPicker)}
-                    className="flex items-center px-1 ml-2 border border-gray-400 rounded-lg cursor-pointer bg-white/70 opacity-70 hover:opacity-100 dark:bg-gray-900/20 dark:border-gray-600 dark:text-gray-300"
+                    className="flex items-center px-2 ml-2 border border-gray-400 rounded-lg cursor-pointer bg-white/70 opacity-70 hover:opacity-100 dark:bg-pink-900/80 dark:border-red-600 dark:text-gray-300"
                   >
                     <span className="w-3 h-3 mr-1">{ColorPickerIcon}</span>
                     Pick
@@ -460,7 +461,7 @@ export default function Main() {
                 ].map((theme) => (
                   <div
                     key={theme}
-                    className={`cursor-pointer shadow dark:shadow-black/80 shadow-gray-500/20 w-8 h-8 rounded-full ${theme}`}
+                    className={`cursor-pointer shadow dark:shadow-black/20 shadow-gray-500/20 w-8 h-8 rounded-full ${theme}`}
                     onClick={() => {
                       setOptions({
                         ...options,
@@ -572,7 +573,7 @@ export default function Main() {
                 Copy
               </div>
               <div
-                className="flex items-center justify-center px-4 py-2 hover:scale-[1.03] duration-200 text-base lg:text-lg font-semibold bg-pink-600 dark:bg-pink-800 text-pink-200 rounded-lg shadow cursor-pointer border border-pink-600 w-full ml-4"
+                className="flex items-center justify-center px-4 py-2 hover:scale-[1.03] duration-200 text-base lg:text-lg font-semibold bg-pink-600/90 dark:bg-pink-600/90 text-pink-200 rounded-lg shadow cursor-pointer border border-pink-600 w-full ml-4"
                 title="Use Ctrl/Cmd + S to save the image"
                 onClick={saveImage}
               >
@@ -661,7 +662,7 @@ export default function Main() {
       <a
         href="https://twitter.com/thelifeofrishi"
         target="_blank"
-        className="flex items-center mb-2 hover:underline lg:mb-0"
+        className="flex items-center hover:underline"
       >
         <span className="w-5 h-5 mx-1">{TwitterIcon}</span>
         Created by Rishi Mohan
@@ -670,10 +671,19 @@ export default function Main() {
       <a
         href="https://github.com/rishimohan/pika"
         target="_blank"
-        className="flex items-center hover:underline"
+        className="flex items-center mt-2 hover:underline lg:mt-0"
       >
         <span className="w-5 h-5 mx-1">{GithubIcon}</span>
         View Code on Github
+      </a>
+      <span className="hidden px-2 lg:block">-</span>
+      <a
+        href="https://www.buymeacoffee.com/thelifeofrishi"
+        target="_blank"
+        className="flex items-center mt-2 hover:underline lg:mt-0"
+      >
+        <span className="w-5 h-5 mx-1">{CoffeeIcon}</span>
+        Buy me a coffee
       </a>
     </div>
   );
